51d0f24985c468dab16da6f09df4d2fd0446c7d1
whiteDwarff <munstarrrrr@gmail.com>
2025-05-10 15:49:49 +0900

    commit

--- react_basic/src/main.jsx
+++ react_basic/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+/*
+  - id가 root인 element에 react component를 rendering 하겠다~
+  - <App /> component는 최상위 root 
+*/
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
