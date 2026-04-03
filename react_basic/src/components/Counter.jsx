e76776faacdac87419b75cf73c501fc752611440
whiteDwarff <munstarrrrr@gmail.com>
2026-04-03 21:14:56 +0900

    useState, 랜더링 단계 학습

--- react_basic/src/components/Counter.jsx
+++ react_basic/src/components/Counter.jsx
@@ -1,3 +1,19 @@
-export default function Counter() {
-  return <button>Counter</button>;
+import { useState } from 'react';
+
+export default function Counter({ onTotal }) {
+  /**
+   * useState :: 
+   * 컴포넌트별 저장소, 리엑트 hook
+   * 상태값과 setter 함수를 반환한다. 
+   */
+  const [counter, setCounter] = useState(0);
+
+  console.log('Counter')
+
+  const handleCounter = () => {
+    setCounter(counter + 1);
+    if (onTotal)  onTotal();
+  }
+
+  return <button onClick={handleCounter}>Counter: {counter}</button>;
 }
--- react_basic/src/components/Main.jsx
+++ react_basic/src/components/Main.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 import Counter from './Counter';
 
 export default function Main() {
+
+  const [total, setTotal] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  const handleTotal = () => {
+    setTotal(total + 1);
+  }
   return (
     <main>
+      <h2>{total}</h2>
+      <h2>{flag.toString()}</h2>
+      <button onClick={() => setFlag(!flag)}>Toggle</button>
+      <hr />
+
+      <Counter onTotal={handleTotal} />
+      <hr />
+      <Counter onTotal={handleTotal} />
+      <hr />
       <Counter />
     </main>
   );
